e31d84086c8e0203c77605b64eb92a0aec117b00
Aishwary Gupta <guptashark@protonmail.com>
2020-08-15 10:08:04 -0400

    Basic CmakeLists file and Test file.
    Test file becuase we're developing a library,
    not a general application.

---
+++ jt_altitude_server_tests.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(void) {
+
+	cout << "jt_altitude_server_tests" << endl;
+
+	cout << "No tests implemented yet." << endl;
+
+	return 0;
+}
